1c28deced5b3c08780e7f8e7811a78135abc5c1b
Ron Michaeli <ronmicha@post.bgu.ac.il>
2017-04-28 13:33:22 +0300

    Our Changes

--- src/JS.js
+++ src/JS.js
@@ -1,57 +1,81 @@
+//region Enums
+var BoardEntity =
+    {
+        Path: 0,
+        Food: 1,
+        PacMan: 2,
+        Ghost: 3,
+        Obstacle: 4,
+        Bonus: 5
+    };
+var Keys =
+    {
+        Left: 37,
+        Up: 38,
+        Right: 39,
+        Down: 40
+    };
+//endregion
+
+//region Consts
+var COLS = 10, ROWS = 10, MAX_FOOD = 50;
+//endregion
+
+//region Members
 var canvas;
 var lblScore;
 var lblTime;
-var context;
-var shape;
+var canvasContext;
+var pacShape;
 var board;
 var score;
-var pac_color;
-var start_time;
-var time_elapsed;
+var pacColor;
+var startTime;
+var timeElapsed;
 var interval;
+//endregion
 
 function Start()
 {
-
     canvas = document.getElementById("canvas");
     lblScore = document.getElementById("lblScore");
     lblTime = document.getElementById("lblTime");
-    context = canvas.getContext("2d");
-    shape = new Object();
+    canvasContext = canvas.getContext("2d");
+    pacShape = new Object();
     board = new Array();
-
     score = 0;
-    pac_color = "yellow";
-    var cnt = 100;
-    var food_remain = 50;
+    pacColor = "yellow";
+    var remainingBoardCells = ROWS * COLS;
+    var food_remain = MAX_FOOD;
     var pacman_remain = 1;
-    start_time = new Date();
-    for (var i = 0; i < 10; i++)
+    startTime = new Date();
+    for (var row = 0; row < ROWS; row++)
     {
-        board[i] = new Array();
-        for (var j = 0; j < 10; j++)
+        board[row] = new Array();
+        for (var col = 0; col < COLS; col++)
         {
             var randomNum = Math.random();
-            if (randomNum <= 1.0 * food_remain / cnt)
+            if (randomNum <= 1.0 * food_remain / remainingBoardCells)
             {
                 food_remain--;
-                board[i][j] = 1;
+                board[row][col] = BoardEntity.Food;
             }
-            else if (randomNum < 1.0 * (pacman_remain + food_remain) / cnt)
+            else if (randomNum < 1.0 * (pacman_remain + food_remain) / remainingBoardCells)
             {
-                shape.i = i;
-                shape.j = j;
+                pacShape.i = row;
+                pacShape.j = col;
                 pacman_remain--;
-                board[i][j] = 2;
+                board[row][col] = BoardEntity.PacMan;
             }
             else
             {
-                board[i][j] = 0;
+                board[row][col] = BoardEntity.Path;
             }
-            cnt--;
+            remainingBoardCells--;
         }
     }
-    keysDown = {};
+    keysDown = new Object();
+
     addEventListener("keydown", function (e)
     {
         keysDown[e.keyCode] = true;
@@ -60,26 +84,27 @@ function Start()
     {
         keysDown[e.keyCode] = false;
     }, false);
+
     interval = setInterval(UpdatePosition, 250);
 }
 
 function GetKeyPressed()
 {
-    if (keysDown[38])
+    if (keysDown[Keys.Up])
     {
-        return 1;
+        return Keys.Up;
     }
-    if (keysDown[40])
+    if (keysDown[Keys.Down])
     {
-        return 2;
+        return Keys.Down;
     }
-    if (keysDown[37])
+    if (keysDown[Keys.Left])
     {
-        return 3;
+        return Keys.Left;
     }
-    if (keysDown[39])
+    if (keysDown[Keys.Right])
     {
-        return 4;
+        return Keys.Right;
     }
 }
 
@@ -87,89 +112,73 @@ function Draw()
 {
     canvas.width = canvas.width; //clean board
     lblScore.value = score;
-    lblTime.value = time_elapsed;
-    for (var i = 0; i < 10; i++)
+    lblTime.value = timeElapsed;
+    for (var row = 0; row < ROWS; row++)
     {
-        for (var j = 0; j < 10; j++)
+        for (var col = 0; col < COLS; col++)
         {
-            var center = new Object();
-            center.x = i * 60 + 30;
-            center.y = j * 60 + 30;
-            if (board[i][j] == 2)
+            var boardEntityCenter = new Object();
+            boardEntityCenter.x = row * 60 + 30;
+            boardEntityCenter.y = col * 60 + 30;
+            if (board[row][col] == BoardEntity.PacMan)
             {
-                context.beginPath();
-                context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
-                context.lineTo(center.x, center.y);
-                context.fillStyle = pac_color; //color
-                context.fill();
-                context.beginPath();
-                context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
-                context.fillStyle = "black"; //color
-                context.fill();
+                canvasContext.beginPath();
+                canvasContext.arc(boardEntityCenter.x, boardEntityCenter.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
+                canvasContext.lineTo(boardEntityCenter.x, boardEntityCenter.y);
+                canvasContext.fillStyle = pacColor; //color
+                canvasContext.fill();
+
+                canvasContext.beginPath();
+                canvasContext.arc(boardEntityCenter.x + 5, boardEntityCenter.y - 15, 5, 0, 2 * Math.PI); // circle
+                canvasContext.fillStyle = "black"; //color
+                canvasContext.fill();
             }
-            else if (board[i][j] == 1)
+            else if (board[row][col] == BoardEntity.Food)
             {
-                context.beginPath();
-                context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
-                context.fillStyle = "black"; //color
-                context.fill();
+                canvasContext.beginPath();
+                canvasContext.arc(boardEntityCenter.x, boardEntityCenter.y, 15, 0, 2 * Math.PI); // circle
+                canvasContext.fillStyle = "black"; //color
+                canvasContext.fill();
             }
         }
     }
-
-
 }
 
 function UpdatePosition()
 {
-    board[shape.i][shape.j] = 0;
-    var x = GetKeyPressed();
-    if (x == 1)
+    board[pacShape.i][pacShape.j] = BoardEntity.Path;
+
+    switch (GetKeyPressed())
     {
-        if (shape.j > 0)
-        {
-            shape.j--;
-        }
+        case Keys.Up:
+            pacShape.j = pacShape.j > 0 ? pacShape.j - 1 : pacShape.j;
+            break;
+        case  Keys.Down:
+            pacShape.j = pacShape.j < ROWS - 1 ? pacShape.j + 1 : pacShape.j;
+            break;
+        case Keys.Left:
+            pacShape.i = pacShape.i > 0 ? pacShape.i - 1 : pacShape.i;
+            break;
+        case Keys.Right:
+            pacShape.i = pacShape.i < COLS - 1 ? pacShape.i + 1 : pacShape.i;
+            break;
     }
-    if (x == 2)
-    {
-        if (shape.j < 9)
-        {
-            shape.j++;
-        }
-    }
-    if (x == 3)
-    {
-        if (shape.i > 0)
-        {
-            shape.i--;
-        }
-    }
-    if (x == 4)
-    {
-        if (shape.i < 9)
-        {
-            shape.i++;
-        }
-    }
-    if (board[shape.i][shape.j] == 1)
-    {
+
+    if (board[pacShape.i][pacShape.j] == BoardEntity.Food)
         score++;
-    }
-    board[shape.i][shape.j] = 2;
+
+    board[pacShape.i][pacShape.j] = BoardEntity.PacMan;
     var currentTime = new Date();
-    time_elapsed = (currentTime - start_time) / 1000;
-    if (score >= 20 && time_elapsed <= 10)
+    timeElapsed = (currentTime - startTime) / 1000;
+    if (score >= 20 && timeElapsed <= 10)
     {
-        pac_color = "green";
+        pacColor = "green";
     }
-    if (score == 50)
+    if (score == MAX_FOOD)
     {
         window.clearInterval(interval);
         window.alert("Game completed");
     }
     else
-    {
         Draw();
-    }
 }
