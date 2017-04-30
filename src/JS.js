44bfbb53d60ef074f64e991035f21ec6eedd4949
Ron Michaeli <ronmicha@post.bgu.ac.il>
2017-04-30 20:18:06 +0300

    Perfecting ghosts movement

--- src/JS.js
+++ src/JS.js
@@ -73,7 +73,7 @@ var foodsOnBoard;
 var pathsList;
 var numOfGhosts;
 var ghostsArray;
-var prevGhostEntity;
+var prevEntityQueue;
 //endregion
 
 window.onload = Start;
@@ -98,7 +98,7 @@ function InitializeMembers()
     board = new Array();
     pathsList = new Array();
     ghostsArray = new Array();
-    prevGhostEntity = new Array();
+    prevEntityQueue = new Array();
     pacShape = new Object();
     keysDown = new Object();
     score = 0;
@@ -172,7 +172,7 @@ function PositionEntities()
         pathsList.splice(i, 1);
     }
 
-    // Position numOfGhosts
+    // Position ghosts
     for (i = 0; i < numOfGhosts; i++)    // 1-3 ghosts allowed
     {
         switch (i)
@@ -190,7 +190,7 @@ function PositionEntities()
                 row = ROWS - 2;
                 break;
         }
-        prevGhostEntity.push(board[col][row]);
+        prevEntityQueue.push(board[col][row]);
         board[col][row] = BoardEntity.Ghost;
         var ghost = new Object();
         ghost.i = col;
@@ -378,53 +378,10 @@ function MoveGhosts()
         var nextStep = BFS(ghost.i, ghost.j);
         ghost.i = nextStep.i;
         ghost.j = nextStep.j;
-        prevGhostEntity.push(board[ghost.i][ghost.j]);
-        board[ghost.i][ghost.j] = BoardEntity.Ghost;
-        board[originalI][originalJ] = prevGhostEntity[i - prevGhostEntity.length];
-        prevGhostEntity.splice(i - prevGhostEntity.length, 1);
-
-        // if (Math.abs(colDiff) < Math.abs(rowDiff))  // x distance from ghost to Pacman is shorter than y distance
-        // {
-        //     if (colDiff > 0 && GhostCanMove(ghost.i - 1, ghost.j))    // Ghost is located right to Pacman
-        //         ghost.i--;
-        //
-        //     else if (colDiff < 0 && GhostCanMove(ghost.i + 1, ghost.j))    // Ghost is located left to Pacman
-        //         ghost.i++;
-        //
-        //     else if (originalI == ghost.i && originalJ == ghost.j)  // Ghost didn't move. Try to move down or up
-        //     {
-        //         if (GhostCanMove(ghost.i, ghost.j + 1) && GhostCanMove(ghost.i, ghost.j - 1))
-        //         {
-        //             var chance = Math.random();
-        //             ghost.j = chance < 0.5 ? ghost.j + 1 : ghost.j - 1;
-        //         }
-        //         else if (GhostCanMove(ghost.i, ghost.j + 1))
-        //             ghost.j++;
-        //         else
-        //             ghost.j--;
-        //     }
-        // }
-        // else   // y distance from ghost to Pacman is shorter or equal to x distance
-        // {
-        //     if (rowDiff > 0 && GhostCanMove(ghost.i, ghost.j - 1))    // Ghost is located below Pacman
-        //         ghost.j--;
-        //
-        //     else if (rowDiff < 0 && GhostCanMove(ghost.i, ghost.j + 1))    // Ghost is located above Pacman
-        //         ghost.j++;
-        //
-        //     else if (originalI == ghost.i && originalJ == ghost.j)  // Ghost didn't move. Try to move right or left
-        //     {
-        //         if (GhostCanMove(ghost.i + 1, ghost.j) && GhostCanMove(ghost.i - 1, ghost.j))
-        //         {
-        //             var chance = Math.random();
-        //             ghost.i = chance < 0.5 ? ghost.i + 1 : ghost.i - 1;
-        //         }
-        //         else if (GhostCanMove(ghost.i + 1, ghost.j))
-        //             ghost.i++;
-        //         else
-        //             ghost.i--;
-        //     }
-        // }
+        prevEntityQueue.push(board[nextStep.i][nextStep.j]);
+        board[nextStep.i][nextStep.j] = BoardEntity.Ghost;
+        board[originalI][originalJ] = prevEntityQueue[0];
+        prevEntityQueue.splice(0, 1);
     }
 }
 
@@ -444,7 +401,6 @@ function Die()
 
 function BFS(col, row)
 {
-    // window.alert("Pac col: " + pacShape.i + ", Pac row: " + pacShape.j);
     var visited = new Array();
     var queue = new Array();
     var first = new Object();
@@ -456,7 +412,6 @@ function BFS(col, row)
     while (queue.length > 0)
     {
         var current = queue[0];
-        // window.alert("col: " + current.i + ", row: " + current.j);
         queue.splice(0, 1);
         if (board[current.i][current.j] == BoardEntity.PacMan)
         {
@@ -467,14 +422,16 @@ function BFS(col, row)
         for (var i = current.i - 1; i <= current.i + 1; i++)
             for (var j = current.j - 1; j <= current.j + 1; j++)
             {
+                if (Math.abs(current.j - j) == 1 && Math.abs(current.i - i) == 1)   // If it's a diagonal move
+                    continue;
                 var neighbor = new Object();
                 neighbor.i = i;
                 neighbor.j = j;
                 if (GhostCanMove(i, j) && !IsVisited(neighbor, visited))
                 {
                     visited.push(neighbor);
-                    neighbor.parentNode = current;
                     queue.push(neighbor);
+                    neighbor.parentNode = current;
                 }
             }
     }
