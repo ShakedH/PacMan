5f31e66febda7234d9f789c07b46697b2e2e1c22
שקד חזון <hazons@post.bgu.ac.il>
2017-05-08 19:59:28 +0300

    Added Jquery Validation style

--- src/PageJS.js
+++ src/PageJS.js
@@ -31,7 +31,17 @@ $(document).ready(function ()
     SetValidation();
 });
 
+//<editor-fold desc="Validations">
 function SetValidation()
+{
+    SetCustomValidationFunctions();
+    SetValidationErrorDesign();
+    SetSignUpValidator();
+
+    SetSignUpValidator();
+}
+
+function SetCustomValidationFunctions()
 {
     $.validator.addMethod("UserExists", function (userName)
     {
@@ -60,9 +70,55 @@ function SetValidation()
     {
         return /^[a-z]+$/i.test(name);
     }, "Name must contain only letters")
+}
+
+function SetValidationErrorDesign()
+{
+    $('input[type="text"]').tooltipster({ //find more options on the tooltipster page
+        trigger: 'custom', // default is 'hover' which is no good here
+        position: 'top',
+        animation: 'grow'
+    });
+    $('input[type="password"]').tooltipster({ //find more options on the tooltipster page
+        trigger: 'custom', // default is 'hover' which is no good here
+        position: 'top',
+        animation: 'grow'
+    });
+    $('input[type="email"]').tooltipster({ //find more options on the tooltipster page
+        trigger: 'custom', // default is 'hover' which is no good here
+        position: 'top',
+        animation: 'grow'
+    });
+    $('input[type="date"]').tooltipster({ //find more options on the tooltipster page
+        trigger: 'custom', // default is 'hover' which is no good here
+        position: 'top',
+        animation: 'grow'
+    });
+}
+
+function SetSignUpValidator()
+{
 
     $("form[name='SignUp']").validate(
         {
+            // any other options & rules,
+            errorPlacement: function (error, element)
+            {
+                var lastError = $(element).data('lastError'),
+                    newError = $(error).text();
+
+                $(element).data('lastError', newError);
+
+                if (newError !== '' && newError !== lastError)
+                {
+                    $(element).tooltipster('content', newError);
+                    $(element).tooltipster('show');
+                }
+            },
+            success: function (label, element)
+            {
+                $(element).tooltipster('hide');
+            },
             rules: {
                 username: {
                     required: true,
@@ -122,6 +178,7 @@ function SetValidation()
         }
     )
 }
+//</editor-fold>
 
 function OpenDiv(divID)
 {
@@ -135,41 +192,14 @@ function AddUser()
     var form = $("#SignUpForm");
     var validator = form.validate();
     if (!form.valid())
-    {
-        validator.reset();
         return;
-    }
 
     var userName = $("#UserNameSignUp").get(0);
-    // VerifyUserName(userName);
-    // if (!userName.validity.valid)
-    //     return false;
-
     var password = $("#PasswordSignUp").get(0);
-    // VerifyPassword(password);
-    // if (!password.validity.valid)
-    //     return false;
-
     var firstName = $("#FirstNameSignUp").get(0);
-    // VerifyName(firstName);
-    // if (!firstName.validity.valid)
-    //     return false;
-
     var surname = $("#SurnameSignUp").get(0);
-    // VerifyName(surname);
-    // if (!surname.validity.valid)
-    //     return false;
-
     var Email = $("#EmailSignUp").get(0);
-    // VerifyEmail(Email);
-    // if (!Email.validity.valid)
-    //     return false;
-
     var birthDate = $("#BirthDateSignUp").get(0);
-    // if (!birthDate.validity.valid)
-    //     return false;
-
-
     var user = {
         UserName: userName.value,
         Password: password.value,
@@ -246,63 +276,6 @@ function ChangeColorSelected(select)
 function ResetValidation(input)
 {
     input.setCustomValidity("");
-}
-
-function VerifyUserName(textbox)
-{
-    var userName = textbox.value;
-    if (!userName || userName == '')
-        textbox.setCustomValidity('User Name is required');
-    else if (!/^[a-z0-9]+$/i.test(userName)) // User name is not alpha numeric
-        textbox.setCustomValidity('User name must contain letters or numbers only');
-    else
-    {
-        var exists = Users.some(function (other)
-        {
-            return other.UserName === userName;
-        });
-        if (exists)
-            textbox.setCustomValidity("User name already exists!");
-        else
-            textbox.setCustomValidity("");
-    }
-}
-
-function VerifyPassword(textbox)
-{
-    var password = textbox.value;
-    if (!password || password == '')
-        textbox.setCustomValidity('Password is required');
-    else if (!(/\d/.test(password)))
-        textbox.setCustomValidity('Password must contain digits');
-    else if (!(/[a-zA-Z]/.test(password)))
-        textbox.setCustomValidity('Password must conatin english characters');
-    else if (password.length < 8)
-        textbox.setCustomValidity('Password must be at least 8 characters long');
-    else
-        textbox.setCustomValidity('');
-}
-
-function VerifyName(textbox)
-{
-    name = textbox.value;
-    if (!name || name == '')
-        textbox.setCustomValidity('First and surname are required');
-    else if (!/^[a-zA-Z()]+$/.test(name))
-        textbox.setCustomValidity('Name can must contain letters only');
-    else
-        textbox.setCustomValidity('');
-}
-
-function VerifyEmail(textbox)
-{
-    var email = textbox.value;
-    if (!email || email == '')
-        textbox.setCustomValidity('Email is required');
-    else if (!/^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{1,4}\b$/i.test(email))
-        textbox.setCustomValidity('Please enter valid Email address');
-    else
-        textbox.setCustomValidity('');
 }
 
 function VerifyUsernameLogin(textbox)
